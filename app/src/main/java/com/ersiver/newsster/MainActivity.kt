5f2b004aa14ef446b08ba21cb9f872363b1c7bb2
Bogusia Reiter <breiter85@gmail.com>
2020-09-12 21:18:10 +0100

    removed redundant import

--- app/src/main/java/com/ersiver/newsster/MainActivity.kt
+++ app/src/main/java/com/ersiver/newsster/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.ersiver.newsster
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.preference.ListPreference
 import androidx.preference.PreferenceManager
 import com.ersiver.newsster.databinding.ActivityMainBinding
-import com.ersiver.newsster.util.PREF_LANGUAGE_KEY
 import com.ersiver.newsster.util.PREF_MODE_KEY
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
-import java.util.*
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +22,7 @@ class MainActivity : AppCompatActivity() {
          * when this method is called for the first time.
          */
         PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false)
-
         setupMode()
-
     }
 
     /**
