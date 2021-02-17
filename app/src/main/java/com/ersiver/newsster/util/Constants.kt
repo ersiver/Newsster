129adf5b1f084ad15d3c518693f2f3c0d30d49d5
Bogusia Reiter <breiter85@gmail.com>
2021-02-17 15:17:54 +0000

    Dagger Hilt update to v2.31

--- app/src/main/java/com/ersiver/newsster/util/Constants.kt
+++ app/src/main/java/com/ersiver/newsster/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.ersiver.newsster.util
 
-const val API_KEY = "079dac74a5f94ebdb990ecf61c8854b7" //Obtain your api key
+const val API_KEY = "079dac7a5f94ebdb990ecf61c8854b7" //Obtain your api key
 
 const val BASE_URL = "https://newsapi.org/v2/"
 
